df1c414c84f265705bdf5487a6b9226e0420a8b9
songjie55 <ooo88888@live.cn>
2020-11-05 14:26:10 +0800

    修改热更

--- server/server.js
+++ server/server.js
@@ -10,31 +10,38 @@ const complier = webpack(webpackConfig)
 const PassThrough = require('stream').PassThrough
 const app = new Koa()
 app.use(json())
-app.use(async (ctx, next) => {
-  const middleware = webpackDevMiddleware(complier, {
-    publicPath: '/__build__/'
-  })
-  await middleware(ctx.req, {
-    end: (content) => {
-      ctx.body = content
-    },
-    setHeader: (name, value) => {
-      ctx.set(name, value)
-    }
-  }, next)
-})
-app.use(async (ctx, next) => {
-  const middleware = webpackHotMiddleware(complier)
-  let stream = new PassThrough()
-  ctx.body = stream
-  await middleware(ctx.req, {
-    write: stream.write.bind(stream),
-    writeHead: (status, headers) => {
-      ctx.status = status
-      ctx.set(headers)
-    }
-  }, next)
-})
+const devMiddleware = (compiler, opts) => {
+  const middleware = webpackDevMiddleware(compiler, opts)
+  return async (ctx, next) => {
+    await middleware(ctx.req, {
+      end: (content) => {
+        ctx.body = content
+      },
+      setHeader: (name, value) => {
+        ctx.set(name, value)
+      }
+    }, next)
+  }
+}
+app.use(devMiddleware(complier))
+const PassThrough = require('stream').PassThrough
+const hotMiddleware = (compiler, opts) => {
+  const middleware = webpackHotMiddleware(compiler, opts)
+  return async (ctx, next) => {
+    let stream = new PassThrough()
+    ctx.body = stream
+    await middleware(ctx.req, {
+      write: stream.write.bind(stream),
+      writeHead: (status, headers) => {
+        ctx.status = status
+        ctx.set(headers)
+      }
+    }, next)
+  }
+
+}
+app.use(hotMiddleware(complier))
+
 app.use(require('koa-static')(__dirname + '/public'))
 app.use(bodyParser({
   enableTypes: ['json', 'form', 'text']
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const fs = require('fs')
+const webpack = require('webpack')
 module.exports = {
   mode: 'development',
   entry: fs.readdirSync(__dirname).reduce((entries, dir) => {
     const fullDir = path.join(__dirname, dir)
     const entry = path.join(fullDir, 'app.ts')
     if (fs.statSync(fullDir).isDirectory() && fs.existsSync(entry)) {
-      entries[dir] = ['webpack-hot-middleware/client',entry]
+      entries[dir] = ['webpack-hot-middleware/client', entry]
     }
     return entries
   }, {}),
@@ -29,6 +30,9 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['.ts', '.tsx','.js']
-  }
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
